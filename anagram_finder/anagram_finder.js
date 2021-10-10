d6f2bbaf8df7467e75d1334f21161da1a8344b91
ByAnthony <anthony.byledbal@icloud.com>
2021-10-10 15:58:56 +0100

    added anagram

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,14 @@
 const AnagramFinder = function (word) {
-
+    this.word = word;
 }
 
-AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
-}
+AnagramFinder.prototype.findAnagrams = function(otherWords) {
+    return otherWords.filter((otherWord) => {
+        if (this.word.toLowerCase().split('').sort().join('') === otherWord.toLowerCase().split('').sort().join('') 
+        && this.word.toLowerCase() !== otherWord.toLowerCase()){
+            return this.word.toLowerCase().split('').sort().join('') === otherWord.toLowerCase().split('').sort().join('');
+        }
+    });
+  }
 
 module.exports = AnagramFinder;
